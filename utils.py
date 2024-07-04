2a99cdb84b003038e59aaac9ea56aa957f278ddc
huangshihyun <yun990065@gmail.com>
2024-07-05 00:49:42 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import requests
 
 def fetch_news_data(query, api_key):
-    url = f"https://newsapi.org/v2/everything?q={query}&apiKey={api_key}"
-    response = requests.get(url)
+    url = 'https://newsapi.org/v2/everything'
+    params = {
+        'q': query,
+        'apiKey': api_key,
+        'language': 'zh',
+        'sortBy': 'publishedAt'
+    }
+    response = requests.get(url, params=params)
     return response.json()
-
-def generate_gmini_story(prompt, user_id, api_key):
-    # 實現 Gmini API 故事生成邏輯
-    pass
