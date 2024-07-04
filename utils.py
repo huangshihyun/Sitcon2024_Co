47e4485191d4d91ba27c7325e11a364aabcfc2bd
huangshihyun <yun990065@gmail.com>
2024-07-05 00:59:43 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
+# utils.py
 import requests
 
-def fetch_news_data(query, api_key):
+def fetch_news_data(query, api_key, num_articles=5):
     url = 'https://newsapi.org/v2/everything'
     params = {
         'q': query,
         'apiKey': api_key,
         'language': 'zh',
-        'sortBy': 'publishedAt'
+        'sortBy': 'publishedAt',
+        'pageSize': num_articles  # 限制返回的文章数量
     }
     response = requests.get(url, params=params)
     return response.json()
-import google.generativeai as genai
-
-
