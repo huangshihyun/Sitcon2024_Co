1c4622bb95273b8121dbd095cfe11ec0291d190e
huangshihyun <yun990065@gmail.com>
2024-07-05 10:41:54 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,9 @@
 # utils.py
 import requests
 
-def fetch_news_data(query, api_key, num_articles=5):
-    url = 'https://newsapi.org/v2/everything'
-    params = {
-        'q': query,
-        'apiKey': api_key,
-        'language': 'zh',
-        'sortBy': 'publishedAt',
-        'pageSize': num_articles  # 限制返回的文章数量
-    }
-    response = requests.get(url, params=params)
-    return response.json()
+import requests
+url = ('https://newsapi.org/v2/top-headlines?'
+       'sources=bbc-news&'
+       'apiKey=API_KEY')
+response = requests.get(url)
+print response.json()
